600c86094da1a89e942d6b614e7822942e747e7e
Fassih Fayyaz <fassihfayyaz@gmail.com>
2023-08-21 12:49:14 +0500

    some more changes

--- install.sh
+++ install.sh
@@ -20,6 +20,17 @@ sudo nala install -y xorg xbacklight xbindkeys xvkbd xinput build-essential
 # Create folders in user directory (eg. Documents,Downloads,etc.)
 xdg-user-dirs-update
 
-
+# Install Qtile Window Manager
 bash /home/$username/debian-config/scripts/qtile-commands
 
+# My Favs
+
+sudo nala install thunar, alacritty, picom, firefox-esr, rofi lightdm lightdm-gtk-greeter-settings -y
+
+# XFCE4 Minimal
+# sudo apt install -y xfce4 xfce4-goodies
+
+# copy my configuration files into the ~/.config directory
+cp dotconfig/* /home/$username/.config/
+
+sudo systemctl enable lightdm
