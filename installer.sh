66c7c4ed5f0bf3d87dc5dc0e04533c37bce7c10a
Fassih Fayyaz <fassihfayyaz@gmail.com>
2023-08-22 22:42:23 +0500

    removed vs code installation

--- installer.sh
+++ installer.sh
@@ -56,16 +56,6 @@ cd Nordzy-cursors
 ./install.sh
 rm -rf Nordzy-cursors
 
-# Install VSCode
-sudo apt-get install wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gpg
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
-
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
 systemctl set-default graphical.target
