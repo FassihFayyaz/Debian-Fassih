a8bad6445a35fac17423afc016a62e4fa3fb883d
Fassih Fayyaz <fassihfayyaz@gmail.com>
2023-08-22 22:11:54 +0500

    Uploaded New Configs

---
+++ dotconfig/qtile/autostart.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash 
+
+if [[ $(systemd-detect-virt) = "none" ]]; then
+    echo "Not running in a Virtual Machine";
+elif xrandr | grep "1366x768"; then
+    xrandr -s 1366x768 || echo "Cannot set 1366x768 resolution.";
+elif xrandr | grep "1920x1080"; then
+    xrandr -s 1920x1080 || echo "Cannot set 1920x1080 resolution.";
+else echo "Could not set a resolution."
+
+picom &
+copyq &
+dunst &
+nitrogen --restore &
