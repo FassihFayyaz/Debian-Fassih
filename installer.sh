6b38212b1225ff52db7a646995ea4007d5d251b0
Fassih Fayyaz <fassihfayyaz@gmail.com>
2023-08-23 01:05:56 +0500

    added build dir and fixed installer

--- installer.sh
+++ installer.sh
@@ -3,15 +3,24 @@
 # Default packages are for the configuration and corresponding .config folders
 # Install packages after installing base Debian with no GUI
 
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
 # Update packages list and update system
-sudo apt update
-sudo apt upgrade -y
+apt update
+apt upgrade -y
 
 # Install nala
 apt install nala -y
 
 # Fetch Latest/Fastest Mirrors
-sudo nala fetch --debian bookworm --https-only
+nala fetch --debian bookworm --https-only
 
 # XFCE4 Minimal
 # sudo nala install -y xfce4 xfce4-goodies
@@ -20,6 +29,7 @@ sudo nala fetch --debian bookworm --https-only
 xdg-user-dirs-update
 
 # Making .config and Moving config files and background to Pictures
+cd $builddir
 mkdir -p /home/$username/.config
 mkdir -p /home/$username/.fonts
 mkdir -p /home/$username/Pictures
@@ -28,15 +38,16 @@ cp wallpaper.png /home/$username/Pictures/backgrounds/
 chown -R $username:$username /home/$username
 
 # Installing Essential Programs 
-nala install feh alacritty rofi thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio xorg pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev -y
+nala install feh alacritty rofi dunst copyq thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev -y
 # Installing Other less important Programs
-nala install neofetch arandr flameshot psmisc mangohud lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm -y
+nala install neofetch arandr xrandr flameshot psmisc lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm -y
 
 # Download Nordic Theme
 cd /usr/share/themes/
 git clone https://github.com/EliverLara/Nordic.git
 
 # Installing fonts
+cd $builddir
 nala install fonts-font-awesome -y
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 unzip FiraCode.zip -d /home/$username/.fonts
@@ -54,15 +65,20 @@ git clone https://github.com/alvatip/Nordzy-cursors
 cd Nordzy-cursors
 ./install.sh
 rm -rf Nordzy-cursors
+nala update
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
 systemctl set-default graphical.target
 
 # Install Qtile
+cd $builddir
 sh scripts/qtile-commands
+cd $builddir
+sh scripts/usenala
 
 # copy my configuration files into the ~/.config directory
+cd $builddir
 sudo cp -r dotconfig/* /home/$username/.config/
 
 printf "\e[1;32mYou can now reboot! Thanks you.\e[0m\n"
