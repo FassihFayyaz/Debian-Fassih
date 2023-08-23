526819ae8020f5727bbc3883fc6d20ffc679281b
Fassih Fayyaz <fassihfayyaz@gmail.com>
2023-08-23 15:13:06 +0500

    removed xrandr

--- installer.sh
+++ installer.sh
@@ -40,7 +40,7 @@ chown -R $username:$username /home/$username
 # Installing Essential Programs 
 nala install feh alacritty rofi dunst copyq thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev -y
 # Installing Other less important Programs
-nala install neofetch arandr xrandr flameshot psmisc lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm -y
+nala install neofetch arandr flameshot psmisc lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm -y
 
 # Download Nordic Theme
 cd /usr/share/themes/
